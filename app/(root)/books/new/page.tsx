40e36a58f94d6994b18aab533d372bdf1824b47e
Muzammil <notmuzammil1@gmail.com>
2026-04-05 19:37:23 +0500

    create 'add new' page

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import UploadForm from '@/components/UploadForm'
 
 const page = () => {
   return (
@@ -10,6 +11,7 @@ const page = () => {
                     upload a PDF to generate your Interactive Interview
                 </p>
             </section>
+            <UploadForm></UploadForm>
         </div>
     </main>
   )
--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -1,8 +1,82 @@
-import React from 'react'
+"use client"
+import { ACCEPTED_IMAGE_TYPES, ACCEPTED_PDF_TYPES, DEFAULT_VOICE } from '@/lib/constants';
+import { UploadSchema } from '@/lib/zod';
+import { BookUploadFormValues } from '@/types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ImageIcon, Upload } from 'lucide-react';
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+import LoadingOverlay from './LoadingOverlay';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import FileUploader from './FileUploader';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 const UploadForm = () => {
+    const [isSubmitting, setisSubmitting] = useState(false);
+    const [isMounted, setisMounted] = useState(false);
+
+    useEffect(()=>{
+        setisMounted(true);
+    }, [])
+
+    const form = useForm<BookUploadFormValues>({
+        resolver: zodResolver(UploadSchema),
+        defaultValues: {
+            title: '',
+            author: '',
+            voice: DEFAULT_VOICE
+        }
+    });
+
+    const onSubmit = async (values: BookUploadFormValues)=>{
+        setisSubmitting(true);
+        await new Promise((resolve)=> setTimeout(resolve, 3000));
+        setisSubmitting(false);
+    }
+
+    if (!isMounted) return null;
+
   return (
-    <div>UploadForm</div>
+    <>
+        {isSubmitting && <LoadingOverlay/>}
+
+        <div className="new-book-wrapper">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+                    <FileUploader control={form.control} name="bookFile" label="Book PDF File" acceptTypes={ACCEPTED_PDF_TYPES} icon={Upload} placeholder="Click to Upload PDF" hint="PDF File (max 50MB)" disabled={isSubmitting}></FileUploader>
+                    <FileUploader control={form.control} name="coverImage" label="Cover Image (optional)" acceptTypes={ACCEPTED_IMAGE_TYPES} icon={ImageIcon} placeholder="Click to Upload Cover Image" hint="leave empty to auto generate from PDF" disabled={isSubmitting}></FileUploader>
+
+                    <FormField control={form.control} name="title" render={({ field })=>{
+                        return <FormItem>
+                            <FormLabel className="form-label">Title</FormLabel>
+                            <FormControl>
+                                <Input className="form-input" placeholder="ex: Rich Dad Poor Dad" {...field} disabled={isSubmitting}></Input>
+                            </FormControl>
+                            <FormMessage></FormMessage>
+                        </FormItem>
+                    }}></FormField>
+
+                    <FormField control={form.control} name="author" render={({ field })=>{
+                        return <FormItem>
+                            <FormLabel className="form-label">Author Name</FormLabel>
+                            <FormControl>
+                                <Input className="form-input" placeholder="ex: Robert Kiyosaki" {...field} disabled={isSubmitting}></Input>
+                            </FormControl>
+                            <FormMessage></FormMessage>
+                        </FormItem>
+                    }}></FormField>
+
+
+                    <Button type="submit" className="form-btn" disabled={isSubmitting}>
+                        Begin Synthesis
+                    </Button>
+
+                </form>
+            </Form>
+        </div>
+
+    </>
   )
 }
 
